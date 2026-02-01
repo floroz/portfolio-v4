aa9ef3c3e85bf3a99ef72102d7c80cc4317df065
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2026-02-01 09:50:39 +0100

    chore: use docker volume to avoid overwriting binaries

--- scripts/playwright-docker.sh
+++ scripts/playwright-docker.sh
@@ -9,8 +9,12 @@ IMAGE="mcr.microsoft.com/playwright:${PLAYWRIGHT_VERSION}-noble"
 
 echo "Running Playwright tests in Docker container: $IMAGE"
 
+# Create a named volume for node_modules to avoid overwriting local binaries
+VOLUME_NAME="portfolio-v4-playwright-node-modules"
+
 docker run --rm -it \
   -v "$(pwd):/work" \
+  -v "${VOLUME_NAME}:/work/node_modules" \
   -w /work \
   --ipc=host \
   "$IMAGE" \
